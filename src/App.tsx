8f5e4b3697e183ea69dc598a7a1695c6fa25e240
mammadovailaha <ilamemmedova14@gmail.com>
2025-10-22 09:00:01 +0400

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import MouseFollowerDot from "./components/MouseFolloerDot";
-import { ThemeProvider } from "./components/ThemeContext";
 import Footer from "./layout/Footer";
 import Navbar from "./layout/Navbar";
 import AppRouter from "./router/AppRouter";
@@ -8,12 +7,10 @@ import AppRouter from "./router/AppRouter";
 function App() {
   return (
     <div className="overflow-x-hidden">
-      <ThemeProvider>
         <Navbar />
         <AppRouter />
         <Footer />
         <MouseFollowerDot />
-      </ThemeProvider>
     </div>
   );
 }
