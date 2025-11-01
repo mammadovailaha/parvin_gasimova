1552a3f72e119244ac5f5eab052aa2dd64ff217d
mammadovailaha <ilamemmedova14@gmail.com>
2025-11-01 19:41:39 +0400

    hero

--- src/components/AboutSection/AboutHero.tsx
+++ src/components/AboutSection/AboutHero.tsx
@@ -4,10 +4,10 @@ import mainPhoto from "../../assets/images/pp about.png";
 
 const AboutHero = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-8 w-full p-10 mt-15 ">
-      <div className="flex flex-col-reverse md:flex-row justify-around items-center gap-5">
-        <div className="w-full md:[40%] lg:w-[45%] flex flex-col gap-4">
-          <p className="font-normal font-poppins text-base md:text-2xl text-[#1E1E1E]">
+    <div className="flex flex-col justify-center items-center gap-2.5 md:gap-8 w-full p-4 md:p-10 mt-15 ">
+      <div className="flex justify-around items-center gap-2.5 md:gap-5">
+        <div className="w-[42%] md:[40%] lg:w-[45%] flex flex-col gap-4">
+          <p className="font-normal font-poppins text-[9px] leading-2.5 md:leading-8 md:text-2xl text-[#1E1E1E]">
             <b className="font-poppins font-semibold">Salam! Mən Pərvin Qasımova, qrafik dizaynerəm. </b>Artıq 2 ildir bu
             sahədə çalışıram və yalnız qrafik dizaynla məhdudlaşmıram — eyni
             zamanda motion dizayner kimi də fəaliyyət göstərirəm. <br />
@@ -17,7 +17,7 @@ const AboutHero = () => {
             estetik, yadda qalan nəticələr yaratmaqdır. 
           </p>
         </div>
-        <div className="w-[250px] md:w-[55%] lg:w-[30%] h-[250px] lg:h-[440px] rounded-lg overflow-hidden">
+        <div className="w-[140px]  md:w-[55%] lg:w-[30%] h-[140px] md:h-[250px] lg:h-[440px] rounded-lg overflow-hidden">
           <img
             className="w-full h-full object-cover rounded-lg"
             src={mainPhoto}
